240b364e7022fefe3ed314694792a08b10e90ba2
Mason <davismasont@gmail.com>
2022-12-02 17:06:24 -0800

    add initial files for react app, and test functionality

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function header() {
+   
+    return (
+        <div>
+        <h1>Mason Davis's Portfolio</h1>
+        <nav>
+            <ul>
+                <li><a href="#content">Projects</a></li>
+                <li><a href="#about-me">about me</a></li>
+                <li><a href="#contact">contact</a></li>
+            </ul>
+        </nav>
+        </div>
+    );
+  }
