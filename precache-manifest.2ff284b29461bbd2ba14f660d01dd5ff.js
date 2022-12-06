6977cc599061bdc626fe25127f71376b842e0c10
Mason <davismasont@gmail.com>
2022-12-05 19:23:29 -0800

    Updates

---
+++ precache-manifest.2ff284b29461bbd2ba14f660d01dd5ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "45575843d60d94fe0a61",
+    "url": "/Portfolio-Mason-Davis/static/css/main.34369f04.chunk.css"
+  },
+  {
+    "revision": "45575843d60d94fe0a61",
+    "url": "/Portfolio-Mason-Davis/static/js/main.88464f35.chunk.js"
+  },
+  {
+    "revision": "3464d9461fabb482585a",
+    "url": "/Portfolio-Mason-Davis/static/js/runtime~main.7422114b.js"
+  },
+  {
+    "revision": "0362e4cd94f0532b0ccd",
+    "url": "/Portfolio-Mason-Davis/static/css/2.2fe86ca6.chunk.css"
+  },
+  {
+    "revision": "0362e4cd94f0532b0ccd",
+    "url": "/Portfolio-Mason-Davis/static/js/2.e4b65f8a.chunk.js"
+  },
+  {
+    "revision": "9cf8e29fa963bdc90033dbec9684b6c8",
+    "url": "/Portfolio-Mason-Davis/static/media/portrait.9cf8e29f.jpg"
+  },
+  {
+    "revision": "5e1fbae303a4705a164e05cc832ccb34",
+    "url": "/Portfolio-Mason-Davis/static/media/weather-port.5e1fbae3.png"
+  },
+  {
+    "revision": "80e30ff95c00be954d8503639c0febf8",
+    "url": "/Portfolio-Mason-Davis/static/media/travel-port.80e30ff9.png"
+  },
+  {
+    "revision": "93855e70a59a0f285951d7aa72d70c66",
+    "url": "/Portfolio-Mason-Davis/static/media/blog-port.93855e70.png"
+  },
+  {
+    "revision": "892c6063e007b5f0746f7852ba39be83",
+    "url": "/Portfolio-Mason-Davis/static/media/game-port.892c6063.png"
+  },
+  {
+    "revision": "8718c744b07e7232ed13d62f9c75c50f",
+    "url": "/Portfolio-Mason-Davis/static/media/notes-port.8718c744.png"
+  },
+  {
+    "revision": "d14b1abc7e33e13f127ee6d275f4bce4",
+    "url": "/Portfolio-Mason-Davis/static/media/tbe-port.d14b1abc.png"
+  },
+  {
+    "revision": "6a299e097f19882ebce884f86bfd6091",
+    "url": "/Portfolio-Mason-Davis/static/media/resume.6a299e09.docx"
+  },
+  {
+    "revision": "224c4d2a7a9591593a7270cd888c6c0f",
+    "url": "/Portfolio-Mason-Davis/index.html"
+  }
+];
